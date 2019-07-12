0efe6ad0171b1c0122fd790e790e417ba9e630db
cassiolinden <cassiolinden@hotmail.com>
2019-07-12 13:56:37 -0300

    Ajustes do pom.xml para execução dos testes fora do Eclipse + execução paralela

--- src/main/java/br/rs/cassiolinden/core/BaseTest.java
+++ src/main/java/br/rs/cassiolinden/core/BaseTest.java
@@ -1,20 +1,35 @@
 package br.rs.cassiolinden.core;
 import static br.rs.cassiolinden.core.DriverFactory.getDriver;
-import static br.rs.cassiolinden.core.DriverFactory.killDriver;
 
 import java.io.File;
 import java.io.IOException;
 
 import org.apache.commons.io.FileUtils;
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.rules.TestName;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
+import br.rs.cassiolinden.pages.LoginPage;
+
 public class BaseTest {
 	@Rule
 	public TestName testname = new TestName();
+	
+	public static LoginPage page = new LoginPage();
+	
+	@Before
+	public void inicializa() {
+		page.acessarTelaInicial();
+		page.logar("cassio@linden", "cassiao");
+	}
+	
+//	@After
+//	public void finaliza() {
+//		DriverFactory.killDriver();
+//	}
 	
 	@After
 	public void finaliza() throws IOException {
@@ -23,7 +38,8 @@ public class BaseTest {
 		FileUtils.copyFile(arquivo, new File("target"+File.separator+"screenshot"+File.separator+testname.getMethodName()+".jpg"));
 		
 		if(Propriedades.FECHAR_BROWSER) {
-			killDriver();
+			DriverFactory.killDriver();
 		}
+		
 	}
 }
